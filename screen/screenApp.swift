80c3adc6fc92401b07a4f4ef0bdd0ea8acafe7a6
Shreyas Venadan <105024953+shreyasvenadan@users.noreply.github.com>
2025-09-14 13:25:49 +0800

    Inital Project

---
+++ screen/screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenApp.swift
+//  screen
+//
+//  Created by Shreyas Venadan on 14/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
